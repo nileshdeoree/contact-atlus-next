081cd6bf8e3b27d5654c1b233738ff936da78ba6
nilesh <deorenilesh@kbtcoe.org>
2023-03-12 14:07:13 +0530

    MONGO_URI added to moongoose.js

--- middleware/mongoose.js
+++ middleware/mongoose.js
@@ -4,7 +4,7 @@ const connectDb = handler => async (req, res)=>{
     if(mongoose.connections[0].readyState){
         return handler (req, res)
     }
-    await mongoose.connect(process.env.MONGO_URI)
+    await mongoose.connect("mongodb+srv://nileshdeore:12501250@cluster0.qogyo0p.mongodb.net/Portfolio?retryWrites=true&w=majority")
     return handler(req, res);
 }
 
